0263119b6096ad701714d7e221a4a09b3989108d
Lucas Tindall <ltindall@ucsd.edu>
2017-05-02 22:55:48 -0700

    Added pid code, and new radio code

--- firmware/quad_firmware/quad_firmware.ino
+++ firmware/quad_firmware/quad_firmware.ino
@@ -1,21 +1,166 @@
 #include <radio.h>
+#include <Adafruit_Simple_AHRS.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h> 
 
-int throttlePin = 19; 
+
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+Adafruit_Simple_AHRS ahrs(&lsm.getAccel(), &lsm.getMag());
+
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+}
+
+
+
+typedef struct{
+  int magic; 
+  int throttle; 
+  int yaw; 
+  int roll; 
+  int pitch; 
+} Controller; 
+
+Controller control_signals; 
+
+// set motor pins 
+int motor1Pin = 19; 
+int motor2Pin = 9; 
+int motor3Pin = 8; 
+int motor4Pin = 5;
+
+// initialize gimbal values 
+long yaw_target = 0;           
+long throttle = 0;
+long roll_target = 0;
+long pitch_target = 0; 
+
+
+// imu readings
+int roll_read = 0;  
+int pitch_read = 0;
+int yaw_read = 0; 
+
+// pid err
+int roll_err = 0; 
+int roll_prev_err = 0; 
+int pitch_err = 0; 
+int pitch_prev_err = 0; 
+int yaw_err = 0; 
+int yaw_prev_err = 0; 
+
+// pid integral
+int roll_integ = 0; 
+int pitch_integ = 0; 
+int yaw_integ = 0; 
+
+// pid derivatives
+int roll_deriv = 0; 
+int pitch_deriv = 0; 
+int yaw_deriv = 0; 
+
+
+// pid variables
+double kp = 1; 
+double ki = 0.5; 
+double kd = 1; 
+
+
+// pid motor settings 
+double roll_motor = 0; 
+double pitch_motor = 0; 
+double yaw_motor = 0; 
+
+
+ 
 unsigned char data = 0; 
 
 void setup() {
   Serial.begin(9600);
   rfBegin(23);
-  pinMode(throttlePin, OUTPUT); 
- 
+  pinMode(motor1Pin, OUTPUT); 
+  setupSensor(); 
 }
 
-void loop() {
-  if (rfAvailable()){
-    data = rfRead(); 
-    analogWrite(throttlePin, data);
-    Serial.println(data); 
+void printIMU()
+{
+
+  Serial.print("IMU roll = "); 
+  Serial.println(roll_read); 
+  Serial.print("IMU pitch = "); 
+  Serial.println(pitch_read); 
+  Serial.print("IMU yaw = "); 
+  Serial.println(yaw_read); 
+}
+
+void pid()
+{ 
+  roll_err = roll_target - roll_read;
+  roll_integ = (roll_integ/2) + roll_err;
+  roll_deriv = roll_err - roll_prev_err;
+  roll_prev_err = roll_err; 
+  roll_motor = (kp*roll_err) + (ki*roll_integ) + (kd+roll_deriv);
+
+
+  pitch_err = pitch_target - pitch_read;
+  pitch_integ = (pitch_integ/2) + pitch_err;
+  pitch_deriv = pitch_err - pitch_prev_err;
+  pitch_prev_err = pitch_err; 
+  pitch_motor = (kp*pitch_err) + (ki*pitch_integ) + (kd+pitch_deriv);
+
+  yaw_err = yaw_target - yaw_read;
+  yaw_integ = (yaw_integ/2) + yaw_err;
+  yaw_deriv = yaw_err - yaw_prev_err;
+  yaw_prev_err = yaw_err; 
+  yaw_motor = (kp*yaw_err) + (ki*yaw_integ) + (kd+yaw_deriv);
+}
+
+void adjust_motors()
+{
+  // TODO: scale down roll_motor, pitch_motor, and yaw_motor
+  analogWrite(motor1Pin, throttle + roll_motor - yaw_motor);
+  analogWrite(motor2Pin, throttle + pitch_motor + yaw_motor); 
+  analogWrite(motor3Pin, throttle - roll_motor - yaw_motor); 
+  analogWrite(motor4Pin, throttle - pitch_motor + yaw_motor); 
+  
+}
+
+void getIMU()
+{ 
+
+  sensors_vec_t orientation;
+  ahrs.getOrientation(&orientation); 
+
+  roll_read = orientation.roll; 
+  pitch_read = orientation.pitch; 
+  yaw_read = orientation.heading; 
+  
+}
+
+void getRadio()
+{
+  if(rfAvailable()) {
+    char bytesRead = rfRead((uint8_t*)&control_signals, sizeof(Controller)); 
+    if(bytesRead == 10 && control_signals.magic == 0xBEAF) { 
+      yaw_target = control_signals.yaw; 
+      throttle = control_signals.throttle; 
+      roll_target = control_signals.roll; 
+      pitch_target = control_signals.pitch;  
+    }
   }
+  
+}
+void loop() {
 
-
+  getRadio(); 
+  getIMU(); 
+  pid(); 
+  adjust_motors(); 
+  
 }
--- firmware/remote_firmware/remote_firmware.ino
+++ firmware/remote_firmware/remote_firmware.ino
@@ -1,54 +1,51 @@
 #include <serLCD.h>
 #include <radio.h>
 
+
+typedef struct{
+  int magic; 
+  int throttle; 
+  int yaw; 
+  int roll; 
+  int pitch; 
+} Controller; 
+
+Controller control_signals; 
+serLCD lcd;
+
+
+// Set pin values 
 int yawPin = 0;
 int throttlePin = 1;
 int rollPin = 2;
 int pitchPin = 3;
 
-long yaw = 0;           // variable to store the value read
+// Initialize gimbal values 
+long yaw = 0;           
 long throttle = 0;
 long roll = 0;
 long pitch = 0;
 
-long throttlePWM = 0; 
-long yawPWM  = 0; 
-long rollPWM = 0; 
-long pitchPWM = 0; 
 
-serLCD lcd;
-//
-// throttle max = 864
+
+// Gimbals max/min settings 
 long throttleMax = 864; 
-// throttle min = 127
 long throttleMin = 127; 
-// yaw max = 840
+
 long yawMax = 840; 
-// yaw min = 94
 long yawMin = 94; 
-// roll max = 837
+
 long rollMax = 837; 
-// roll min = 96
 long rollMin = 96; 
-// pitch max = 840
+
 long pitchMax = 840; 
-// pitch min = 94
 long pitchMin = 94; 
 
-// throttle resting = 504
-// 750 - 504 = 246
-// yaw resting = 456
-// 750 - 456 = 294
-// roll resting = 459
-// 750 - 459 = 291
-// pitch resting = 468
-// 750 - 468 = 282
 
-// x0 = 127, x1 = 864, y0 = 0, y1 = 1500
-// x0 = 94, x1 = 840, y0 = 0, y1 = 1500
-// x0 = 96, x1 = 837, y0 = 0, y1 = 1500
-// x0 = 94, x1 = 840, y0 = 1, y1 = 1500
-// y = y0 + (y1 - y0) * (x - x0) / (x1 - x0)
+
+
+
+
 
 long convert( long val, long minA, long maxA, long minB, long maxB)
 { 
@@ -64,107 +61,77 @@ void setup()
   rfBegin(23);
   lcd.home(); 
   lcd.display();
+  
+  control_signals.magic = 0xBEAF; 
 
 }
 
-
-
-void loop()
-
+void print_lcd()
 {
   lcd.clear(); 
-  // throttle
-  // x0 = 127, x1 = 864, y0 = 0, y1 = 1500
-  // yaw
-  // x0 = 94, x1 = 840, y0 = 0, y1 = 1500
-  // roll
-  // x0 = 96, x1 = 837, y0 = 0, y1 = 1500
-  // pitch
-  // x0 = 94, x1 = 840, y0 = 1, y1 = 1500
-  // y = y0 + (y1 - y0) * (x - x0) / (x1 - x0)
-
-  // scale throttle
-  throttle = analogRead(throttlePin);
-  //throttle = convert(throttle, throttleMin, throttleMax, 0, 1500); 
-  //throttle = 1500 * (throttle - 127) / (864 - 127);
+   
   lcd.setCursor(0, 0);
   lcd.print("T:");
   lcd.setCursor(0, 2);
-  lcd.print(convert(throttle, throttleMin, throttleMax, 0, 1500));
+  lcd.print(throttle);
 
-  // scale yaw
-  yaw = analogRead(yawPin);
-  //yaw = convert(yaw, yawMin, yawMax, 0, 1500); 
-  //yaw = 1500  * (yaw - 94) / (830 - 94) ;
   lcd.setCursor(0, 8);
   lcd.print("Y:");
   lcd.setCursor(0, 10);
-  lcd.print(convert(yaw, yawMin, yawMax, 0, 1500));
+  lcd.print(yaw);
 
-
-
-
-  // scale roll
-  roll = analogRead(rollPin);
-  //roll = convert(roll, rollMin, rollMax, 0, 1500); 
-  //roll = 1500 * (roll - 96) / (837 - 96);
   lcd.setCursor(1, 0);
   lcd.print("R:");
   lcd.setCursor(1, 2);
-  lcd.print(convert(roll, rollMin, rollMax, 0, 1500));
-
-
-  // scale pitch
-  pitch = analogRead(pitchPin);
-  //pitch = convert(pitch, pitchMin, pitchMax, 0, 1500);
-  //pitch = 1500 * (pitch - 94) / (840 - 94);
+  lcd.print(roll);
+  
   lcd.setCursor(1, 8);
   lcd.print("P:");
   lcd.setCursor(1, 10);
-  lcd.print(convert(pitch, pitchMin, pitchMax, 0, 1500));
+  lcd.print(pitch);
 
+}
 
-  throttlePWM = convert(throttle, throttleMin, throttleMax, 0, 255); 
-  if(throttlePWM > 255) throttlePWM = 255;
-  else if(throttlePWM < 0) throttlePWM = 0; 
-  //rfWrite('t'); 
-  rfWrite(lowByte(throttlePWM));
+void getGimbals()
+{
+
+  // scale throttle
+  throttle = analogRead(throttlePin);
+  throttle = convert(throttle, throttleMin, throttleMax, 0, 1500); 
   
-  yawPWM = convert(yaw, yawMin, yawMax, 0, 255);
-  if(yawPWM > 255) yawPWM = 255; 
-  else if(yawPWM < 0) yawPWM = 0; 
-  //rfWrite(lowByte(yawPWM));
+  // scale yaw
+  yaw = analogRead(yawPin);
+  yaw = convert(yaw, yawMin, yawMax, 0, 1024); 
 
-  rollPWM = convert(roll, rollMin, rollMax, 0, 255); 
-  if(rollPWM > 255) rollPWM = 255; 
-  else if(rollPWM < 0) rollPWM = 0; 
-  //rfWrite(lowByte(rollPWM));
+  // scale roll
+  roll = analogRead(rollPin);
+  roll = convert(roll, rollMin, rollMax, 0, 1024);  
 
-  pitchPWM = convert(pitch, pitchMin, pitchMax, 0, 255); 
-  if(pitchPWM > 255) pitchPWM = 255; 
-  else if(pitchPWM < 0) pitchPWM = 0; 
-  //rfWrite(lowByte(pitchPWM));
- 
+  // scale pitch
+  pitch = analogRead(pitchPin);
+  pitch = convert(pitch, pitchMin, pitchMax, 0, 1024); 
+}
 
-  Serial.print("yaw = ");
-  Serial.println(yaw);
-  Serial.print("throttle = ");
-  Serial.println(throttle);
-  Serial.print("roll = ");
-  Serial.println(roll);
-  Serial.print("pitch = ");
-  Serial.println(pitch);
-  Serial.println("###########################");
-  Serial.print("yawPWM = ");
-  Serial.println(yawPWM);
-  Serial.print("throttlePWM = ");
-  Serial.println(lowByte(throttlePWM));
-  Serial.print("rollPWM = ");
-  Serial.println(rollPWM);
-  Serial.print("pitchPWM = ");
-  Serial.println(pitchPWM);
-  Serial.println("###########################");
-  //delay(100);
+void sendRadio()
+{
+  control_signals.throttle = throttle; 
+  control_signals.yaw = yaw; 
+  control_signals.pitch = pitch; 
+  control_signals.roll = roll; 
+
+
+  rfWrite((uint8_t*)&control_signals, sizeof(Controller));
+}
+
+void loop()
+{
+
+  print_lcd(); 
+
+  getGimbals(); 
+  
+  sendRadio(); 
+  
 }
 
 
